6e926433271efb7a98721e9701b16991c83e891f
maupassant9 <xiadongengenheiro@yahoo.com.br>
2017-12-29 21:14:08 -0200

    changes in 74hc595

---
+++ gpio_control/74HC595/74HC595.c
@@ -0,0 +1,15 @@
+/*
+ * FileName: 74HC595
+ * Author: Dong Xia
+ * This is source file of the driver of 74HC595
+ *
+ * Change Records:
+ *      >> (29/Dec/2017): Creation of file
+ *
+ */
+ 
+ /********************************************
+  * Include
+  ********************************************/
+  #include <wiringPi.h>
+  
